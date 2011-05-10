4bbc26caf5df9397fe1960cdc9e22213080de99e
Eric Nordmeyer <eric@nordmeyer.name>
2011-05-10 12:08:18 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nordmeyer::Application.config.secret_token = '60761f590f29cfe4c1dde6aec44a67ec80775b936954f2b89c6e000305cfd1a218eecafa5a46c14506b0c9ba9dc96c4429d990ecb51e5cf7909bf3d5ed67fe1b'
